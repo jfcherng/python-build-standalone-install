e5bbdea7a0a5d00379b3ceb744de3d50017df59b
Jack Cherng <jfcherng@gmail.com>
2024-06-25 15:50:56 +0800

    init
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ src/sitecustomize.py
@@ -0,0 +1,20 @@
+import ssl
+
+try:
+    import sitecustomize2  # noqa: F401
+except ImportError:
+    pass
+
+try:
+    import certifi
+
+    # define a function that returns a default SSL context with certifi's CA bundle
+    def create_certifi_context(*args, cafile=None, **kwargs):
+        return ssl_create_default_context_bak(*args, cafile=cafile or certifi.where(), **kwargs)
+
+    # backup the original ssl.create_default_context
+    ssl_create_default_context_bak = ssl.create_default_context
+    # set the default SSL context creation function to use certifi's CA bundle
+    ssl._create_default_https_context = ssl.create_default_context = create_certifi_context
+except Exception as e:
+    print(f"certifi package not installed: {e}")
---
+++ src/source.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+_provide_python() {
+    # don't overwrite the `SCRIPT_DIR` in the parent script
+    local SCRIPT_DIR
+
+    SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+    PATH="${SCRIPT_DIR}/bin:${PATH}"
+    export PATH
+
+    LD_LIBRARY_PATH="${SCRIPT_DIR}/lib:${LD_LIBRARY_PATH}"
+    export LD_LIBRARY_PATH
+}
+
+_provide_python
