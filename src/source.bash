267de61ea8aedb8394d3998ea847c15ec65db51a
Jack Cherng <jfcherng@gmail.com>
2024-10-11 16:40:13 +0800

    feat: support Windows
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- src/source.bash
+++ src/source.bash
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
+# pbs = python build standalone
 
-_provide_python() {
+__pbs_is_windows() {
+    [[ -n ${WINDIR} ]]
+}
+
+__pbs_provide_python() {
     # don't overwrite the `SCRIPT_DIR` in the parent script
     local SCRIPT_DIR
 
     SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-    PATH="${SCRIPT_DIR}/bin:${PATH}"
+    if __pbs_is_windows; then
+        PATH="${SCRIPT_DIR}/Scripts:${SCRIPT_DIR}:${PATH}"
+    else
+        PATH="${SCRIPT_DIR}/bin:${PATH}"
+    fi
     export PATH
 
     CPATH="${SCRIPT_DIR}/include:${CPATH}"
     export CPATH
 
-    LD_LIBRARY_PATH="${SCRIPT_DIR}/lib:${LD_LIBRARY_PATH}"
+    if __pbs_is_windows; then
+        LD_LIBRARY_PATH="${SCRIPT_DIR}/DLLs:${LD_LIBRARY_PATH}"
+    else
+        LD_LIBRARY_PATH="${SCRIPT_DIR}/lib:${LD_LIBRARY_PATH}"
+    fi
     export LD_LIBRARY_PATH
 }
 
-_provide_python
+__pbs_provide_python
